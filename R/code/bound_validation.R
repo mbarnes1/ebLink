a511d5c581ef2bf460fc8c872adf4b8152a3d83b
Matt Barnes <barnes.matt.j@gmail.com>
2016-05-18 14:38:28 -0400

    String experiments

--- R/code/bound_validation.R
+++ R/code/bound_validation.R
@@ -3,7 +3,7 @@ Emperical Validation of Theoretical Lower Bounds
 Author: mbarnes1@cs.cmu.edu
 "
 rm(list=ls())
-setwd('/home/scratch/mbarnes1/ebLink/R/code/')
+setwd('~/Documents/trafficjam/beka/ebLink/R/code/')
 
 compute_errors_wrapper <- function(x) {
   result <- tryCatch({
@@ -55,9 +55,9 @@ compute_errors <- function(param) {
   error_exactsampling <- sum(Lambda != Lambda.exact)/n
   
   # Run experiment (full Gibbs sampler)
-  Lambda.gibbs <- rl.gibbs(file.num=rep(1,n),X.s=X.s,X.c=X.c,num.gs=10000,a=a,b=b,c=c,d=d, M=n, num.gs.burnin=10000, Y.s.0=Y.s, Y.c.0=Y.c)
-  error_gibbs <- sum(Lambda != Lambda.gibbs[nrow(Lambda.gibbs),])/n
-  errors = c(bound_expected_error, error_exactsampling, error_gibbs)
+  #Lambda.gibbs <- rl.gibbs(file.num=rep(1,n),X.s=X.s,X.c=X.c,num.gs=100,a=a,b=b,c=c,d=d, M=n, num.gs.burnin=100, Y.s.0=Y.s, Y.c.0=Y.c)
+  #error_gibbs <- sum(Lambda != Lambda.gibbs[nrow(Lambda.gibbs),])/n
+  errors = c(bound_expected_error, error_exactsampling)
   return(errors)
 }
 
@@ -65,16 +65,20 @@ library(parallel)
 
 
 # Experiment parameters
-n_params = 3
-n_vec <- seq(from = 20, to = 20, length = n_params)
-betas <- seq(from = 0.6, to = 0.6, length = n_params)
-#thetas <- rep(list(list(rep(0.1, 10), rep(0.1, 10), rep(0.1, 10))), n_params)
-thetas <- vector('list', n_params)
-
+n_values = seq.int(from=1.0, to=50, by=5)
+n_params = length(n_values)
+n_vec <- seq(from = 100, to = 100, length = n_params)
+betas <- seq(from = 0.8, to = 0.8, length = n_params)
+thetas <- rep(list(list(rep(0.1, 10), rep(0.1, 10), rep(0.1, 10))), n_params)
+#thetas <- vector('list', n_params)
+S <- read.csv('yob2014.csv', sep=',', header=FALSE)
+S <- as.vector(S[1:20,1])
+thetas_strings = rep(list(list(rep(0.05, 20), rep(0.05, 20))), n_params)
 
 params <- vector('list', n_params)
 for (i in 1:n_params) {
-  thetas[[i]] <- rep(list(c((i-1)*0.1, (1-(i-1)*0.1))), 3)
+  #thetas[[i]] <- rep(list(rep(1/4, 4)), i)  # run with n=100, beta=0.6, nparams=15
+  #thetas[[i]] <- rep(list(rep(1/n_values[i], n_values[i])), 5)
   params[i] <- list(list(betas[i], n_vec[i], thetas[[i]]))
 }
 
@@ -82,7 +86,7 @@ for (i in 1:n_params) {
 a <-1
 b <- 999
 c <- 1
-d <- function(string1,string2){adist(string1,string2)}
+d <- function(S){adist(S)}
   
 # Experiment multiprocessing
 no_cores <- detectCores()
@@ -95,7 +99,7 @@ stopCluster(cl)
 errors = do.call(rbind, errors)
 bound_expected_error = errors[,1]
 error_exactsampling = errors[,2]
-error_gibbs = errors[,3]
+#error_gibbs = errors[,3]
 
 # Print the results
 print('Parameters were (in order beta, n, theta)')
@@ -104,19 +108,19 @@ print('Bound expected error')
 print(bound_expected_error)
 print('Exact sampling error')
 print(error_exactsampling)
-print('Gibbs sampling')
-print(error_gibbs)
+#print('Gibbs sampling')
+#print(error_gibbs)
 
 # Save the results
-save(n_vec, betas, thetas, bound_expected_error, error_exactsampling, error_gibbs, file=paste('results/',paste(format(Sys.time(), "%Y-%m-%d-%H-%M-%S"), "Rdata", sep = "."), sep=''))
+#save(n_vec, betas, thetas, bound_expected_error, error_exactsampling, error_gibbs, file=paste('results/',paste(format(Sys.time(), "%Y-%m-%d-%H-%M-%S"), "Rdata", sep = "."), sep=''))
 
 # Plot the results
-plot(n_vec, error_exactsampling, type="b", col="red", ylim=c(0, max(max(error_gibbs), max(error_exactsampling))), ann=FALSE, pch=21, lwd=4)
-lines(n_vec, bound_expected_error, type='b', col="blue", lty=2, pch=22, lwd=4)
-lines(n_vec, error_gibbs, type='b', col="green", lty=3, pch=23, lwd=4)
+plot(n_values, error_exactsampling, type="b", col="red", ylim=c(0, max(max(error_exactsampling), max(error_exactsampling))), ann=FALSE, pch=21, lwd=4)
+lines(n_values, bound_expected_error, type='b', col="blue", lty=2, pch=22, lwd=4)
+#lines(1:n_params, error_gibbs, type='b', col="green", lty=3, pch=23, lwd=4)
 title(main='Experimental Bound Validation', xlab='n', ylab='Error')
 
-legend('bottomright', c("Exact Sampling","Bound", "Gibbs"), cex=1, 
-       col=c("red","blue", "green"), lty=1:3, pch=21:23, lwd=2, bty="n")
+#legend('bottomright', c("Exact Sampling","Bound", "Gibbs"), cex=1, 
+#       col=c("red","blue", "green"), lty=1:3, pch=21:23, lwd=2, bty="n")
 
 
--- R/code/model.R
+++ R/code/model.R
@@ -52,16 +52,46 @@ model.draw.X.c <- function(Lambda, Y.c, z, thetas) {
     X.c - Observed records (n x numberfields array)
   "
   n <- length(Lambda)
-  Y.c.corruption <- model.draw.Y.c(thetas, n)
+  X.c.corruption <- model.draw.Y.c(thetas, n)
   X.c.uncorrupted <- Y.c[Lambda,]
-  X.c <- X.c.uncorrupted*(1-z) + Y.c.corruption*z
+  X.c <- X.c.uncorrupted*(1-z) + X.c.corruption*z
   return(X.c)
 }
 
-"
+model.draw.X.s <- function(Lambda, Y.s, z, thetas, S, d, c) {
+  "
+  Inputs:
+  Lambda - Linkage structure (vector of length n)
+  Y.s - Latent entities (n x numberfields array)
+  z - Binary corruption array (n x numberfields array)
+  thetas - Field probabilities (numberfields length list of probability vectors, equivalent to G)
+  S - list of strings in dictionary
+  d - string edit distance function
+  Outputs:
+  X.s - Observed records (n x numberfields array)
+  "
+  n <- length(Lambda)
+  m <- length(thetas)
+  D <- d(S)
+  thetas <- do.call(rbind, thetas)
+  X.s.corruption <- matrix(NaN, n, m)
+  for (i in 1:n) {
+    term1 = thetas*exp(-c*t(D[,Y.s[Lambda[i],]]))
+    probs = sweep(term1,1, rowSums(term1), FUN='/')
+    probs = split(t(probs), rep(1:nrow(probs), each=ncol(probs)))
+    X.s.corruption[i,] <- model.draw.Y.c(probs, 1)
+  }
+  X.s.uncorrupted <- Y.s[Lambda,]
+  X.s <- X.s.uncorrupted*(1-z) + X.s.corruption*z
+  return(X.s)
+}
+
+
 # Example
 n = 10  # number of records
-m = 3  # number of fields
+m_s = 2
+m_c = 3
+m = m_c + m_s  # number of fields
 beta = runif(m)
 print(beta)
 z <- model.draw.z(beta, n)
@@ -72,10 +102,20 @@ theta3 = rep(0.1, 10)
 thetas = list(theta1, theta2, theta3)
 Y.c <- model.draw.Y.c(thetas, n)
 print(Y.c)
+S = list('aa', 'bb', 'cc', 'ddhhe', 'ee', 'ab', 'cd', 'ef', 'gh', 'azaz')
+c <- 1
+d <- function(S){adist(S)}
+theta1_string = rep(0.1, 10)
+theta2_string = c(0.5, 0.1, 0.1, 0.05, 0.05, 0.01, 0.02, 0.03, 0.04, 0.1)
+thetas_string = list(theta1_string, theta2_string)
+Y.s <- model.draw.Y.c(thetas_string, n)
+print(Y.s)
 Lambda <- model.draw.Lambda(n)
 print(Lambda)
-X.c <- model.draw.X.c(Lambda, Y.c, z, thetas)
+X.c <- model.draw.X.c(Lambda, Y.c, z[,(m_s+1):ncol(z)], thetas)
+X.s <- model.draw.X.s(Lambda, Y.s, z[,1:m_s], thetas_string, S, d, c)
 print(X.c)
-"
+print(X.s)
+
 
 
